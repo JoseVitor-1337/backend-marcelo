d9e3077ea70b29bfc5e55d08c0c2e99c6f5e40af
JoseVitor-1337 <josevitordemelo@yahoo.com>
2020-08-15 19:57:48 -0400

    fix the yarn prod command error

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import upload from "@config/upload";
 
+import validateAcessInRouter from "./middleware/validateAcessInRouter";
+
 import ParticipantController from "@controllers/ParticipantController";
 import ResearcherController from "@controllers/ResearcherController";
 import AuthenticationController from "@controllers/AuthenticationController";
-import validateAcessInRouter from "./middleware/validateAcessInRouter";
 import AdministerController from "@controllers/AdministerController";
 import UploadsController from "@controllers/UploadsController";
 import SearchController from "@controllers/SearchController";
