ea575cf76c6b00550cac8c160e87e27fc7c50738
JoseVitor-1337 <josevitordemelo@yahoo.com>
2020-08-16 14:49:57 -0400

    fix deploy errors

--- src/config/enviroment.ts
+++ src/config/enviroment.ts
@@ -2,10 +2,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-const API_SECRET = process.env.SECRET || "secret";
-const DB_URL = "mongodb://localhost:27017/pesquisas";
-
-//   process.env.DATABASE_URL ||
+const PORT = process.env.PORT;
+const API_SECRET = process.env.SECRET;
+const DB_URL = process.env.DATABASE_URL;
 
 export { PORT, DB_URL, API_SECRET };
